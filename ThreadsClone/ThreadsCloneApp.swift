860676976db5bd2e447a0731915d7d7b3115fdcf
asadbek-ip <dasturchi6678@gmail.com>
2024-02-28 17:23:07 +0500

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Mac Mini 1 on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
